21a8b7ca81ca5edbda41fe08413597dd36aae854
lcorr8 <l.corr.v@gmail.com>
2017-05-07 16:32:45 +0200

    Done.

--- reducer.js
+++ reducer.js
@@ -1,27 +1,47 @@
-let state;
-function changeState(state = {count: 0}, action){
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
-    }
+//generalizaed code
+
+function createStore(reducer) {
+  let state;
+ //call to dispatch should call a reducer, reassign the state and render a change
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  }
+ 
+  function getState() {
+    return state;
   }
 
-function dispatch(action){
-  state = changeState(state, action)
-  render()
+  dispatch({ type: '@@INIT' });
+ 
+  return { 
+    dispatch, 
+    getState
+  };
+};
+
+//particular to this app  
+
+//how the state should change in response to different actions being dispathced
+function changeCount(state = {count: 0}, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
+    default:
+      return state;
+  }
 }
+//how the DOM is updated
+function render() {
+  let container = document.getElementById('container');
+  container.textContent = store.getState().count;
+};
+ 
+let store = createStore(changeCount);
 
-function render(){
-  let container = document.getElementById('container')
-  container.textContent = state.count
-}
+//what triggers a dispatch method  
+let button = document.getElementById('button');
 
-  dispatch({type: '@@INIT'})
-  
-  let button = document.getElementById('button');
-
-  button.addEventListener('click', function(){
-    dispatch({type: 'INCREASE_COUNT'})
-  })
+button.addEventListener('click', function(){
+  dispatch({type: 'INCREASE_COUNT'})
+})
